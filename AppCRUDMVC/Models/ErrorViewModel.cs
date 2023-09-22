9d093f4c7f3a468ec4584051740f662fa098d2ec
lcordoval <lcordova@multiplied.com>
2023-09-21 20:10:01 -0500

    Agregue un comentario

--- AppCRUDMVC/Models/ErrorViewModel.cs
+++ AppCRUDMVC/Models/ErrorViewModel.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace AppCRUDMVC.Models
 {
+    /// <summary>
+    /// Cambio para ejemplo GIT desde VISUAL STUDIO
+    /// </summary>
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
